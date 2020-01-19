c3da3e7342a95b270e60d0725e3d77f7dd168ba3
Ivan <me@example.com>
2020-01-18 22:48:38 -0600

    Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       // resolve: `..`,
       resolve: `gatsby-source-instagram`,
       options: {
-        username: `fullname7274`,
+        username: `renebaebae`,
       },
     },
     {
